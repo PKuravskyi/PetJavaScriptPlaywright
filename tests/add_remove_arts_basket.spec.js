038deb56a3f9f54177f340c80dc1ca5479b7c005
Pavlo Kuravskyi <pkuravsky@gmail.com>
2024-01-13 21:38:39 +0200

    refactor

--- tests/add_remove_arts_basket.spec.js
+++ tests/add_remove_arts_basket.spec.js
@@ -2,21 +2,21 @@ import { test, expect } from '@playwright/test';
 
 import { ArtsPage } from '../pages/ArtsPage';
 
-let artsPage;
-
-test.beforeEach(async ({ page }) => {
-	artsPage = new ArtsPage(page);
-	await artsPage.visit('http://localhost:2221');
-});
-
 test.describe('Arts page', () => {
-	test('add art to basket', async ({ page }) => {
+	let artsPage;
+
+	test.beforeEach(async ({ page }) => {
+		artsPage = new ArtsPage(page);
+		await artsPage.visit('http://localhost:2221');
+	});
+
+	test('Verify art art can be added to basket', async ({ page }) => {
 		await expect(artsPage.basketCounterEl).toHaveText('0');
 		artsPage.addArtToBasket('Mountain Landscape');
 		await expect(artsPage.basketCounterEl).toHaveText('1');
 	});
 
-	test('remove art from basket', async ({ page }) => {
+	test('Verify art can be removed from basket', async ({ page }) => {
 		await expect(artsPage.basketCounterEl).toHaveText('0');
 		artsPage.addArtToBasket('Mountain Landscape');
 		await expect(artsPage.basketCounterEl).toHaveText('1');
