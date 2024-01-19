32932b0ab6485a65853ce7963a05f7eb3f3f6da2
Pavlo Kuravskyi <pkuravsky@gmail.com>
2024-01-19 15:27:15 +0200

    update pageFixtures

--- pages/pageFixtures.js
+++ pages/pageFixtures.js
@@ -8,16 +8,36 @@ import { DeliveryDetailsPage } from './checkout/DeliveryDetailsPage';
 import { PaymentPage } from './checkout/PaymentPage';
 import { ThankYouPage } from './checkout/ThankYouPage';
 
-exports.expect = base.expect;
 exports.test = base.test.extend({
-	artsPage: async ({ page }, use) => await use(new ArtsPage(page)),
-	basketPage: async ({ page }, use) => await use(new BasketPage(page)),
-	loginPage: async ({ page }, use) => await use(new LoginPage(page)),
-	myAccountPage: async ({ page }, use) => await use(new MyAccountPage(page)),
-	signUpPage: async ({ page }, use) => await use(new SignUpPage(page)),
+	artsPage: async ({ page }, use) => {
+		await use(new ArtsPage(page));
+	},
+
+	basketPage: async ({ page }, use) => {
+		await use(new BasketPage(page));
+	},
+
+	loginPage: async ({ page }, use) => {
+		await use(new LoginPage(page));
+	},
+
+	myAccountPage: async ({ page }, use) => {
+		await use(new MyAccountPage(page));
+	},
+
+	signUpPage: async ({ page }, use) => {
+		await use(new SignUpPage(page));
+	},
+
 	deliveryDetailsPage: async ({ page }, use) => {
 		await use(new DeliveryDetailsPage(page));
 	},
-	paymentPage: async ({ page }, use) => await use(new PaymentPage(page)),
-	thankYouPage: async ({ page }, use) => await use(new ThankYouPage(page)),
+
+	paymentPage: async ({ page }, use) => {
+		await use(new PaymentPage(page));
+	},
+
+	thankYouPage: async ({ page }, use) => {
+		await use(new ThankYouPage(page));
+	},
 });
