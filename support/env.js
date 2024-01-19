2e4219b2edeeb7393d83baa2e6c0033a3fb467e4
Pavlo Kuravskyi <pkuravsky@gmail.com>
2024-01-19 15:26:48 +0200

    combine fixtures

--- support/env.js
+++ support/env.js
@@ -1,3 +1,10 @@
 import 'dotenv/config';
+import { mergeTests } from '@playwright/test';
 
-export default () => {};
+const { test: pageFixtures } = require('../pages/pageFixtures');
+const {
+	test: endpointFixtures,
+} = require('../services/endpoints/endpointFixtures');
+
+const test = mergeTests(pageFixtures, endpointFixtures);
+module.exports = { test };
